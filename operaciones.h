bf36e60d4176841b18f0fa8e79cf762938ded6c3
leortegar <ortegarle@gmail.com>
2015-10-27 20:12:14 -0600

    Se agregan más definiciones en operaciones.h

--- operaciones.h
+++ operaciones.h
@@ -1,21 +1,58 @@
 #include <math.h>
 #include <stdio.h>
 
+
+/*---------------        1. Suma        ------------------*/
 double sum(double a, double b);
+/*---------------       2. Resta        ------------------*/
 double resta(double a, double b);
+/*---------------   3. Multiplicación   ------------------*/
 double mult(double a, double b);
+/*---------------      4. División      ------------------*/
 double div(double a, double b);
+/*---------------        5. Seno        ------------------*/
 double seno(double a);
+/*---------------       6. Coseno       ------------------*/
 double coseno(double a);
+/*---------------      7. Tangente      ------------------*/
 double tangente(double a);
+/*---------------      8. Arco Seno     ------------------*/
 double aseno(double a);
+/*---------------     9. Arco Coseno    ------------------*/
 double acoseno(double a);
+/*---------------   10. Arco Tangente   ------------------*/
 double atangente(double a);
+/*---------------      11. Seno Hip     ------------------*/
 double senoh(double a);
+/*--------------     12. Coseno Hip    -------------------*/
 double cosenoh(double a);
+/*---------------    13. Tangente Hip   ------------------*/
 double tangenteh(double a);
+/*---------------      14. atan2()      ------------------*/
 double atangente2(double a, double b);
+/*---------------      15. Ceil ()      ------------------*/
 int techo(double a);
+/*---------------       16. Exp ()      ------------------*/
 double exponencial(double a);
+/*---------------      17. Floor ()     ------------------*/
 int piso(double a);
+/*---------------      18. fabs ()      ------------------*/
+double valorAbsoluto(double a);
+/*---------------      20. fmod ()      ------------------*/
+double fmod(double a, double b, double resultado);
+/*---------------     21. frexp ()      ------------------*/
+double frexp(double x, double frac);
+/*---------------     22. ldexp ()      ------------------*/
+double Idexp(double x, double ret);
+/*---------------      23. log ()       ------------------*/
+double logaritmo(double a);
+/*---------------     24. log10 ()      ------------------*/
+double logBase10(double a);
+/*---------------      25. modf ()      ------------------*/
+double modf(double x, double pfrac, double pentera);
+/*---------------       26. pow ()      ------------------*/
+double potencia(double a, double b);
+/*---------------      27. sqrt ()      ------------------*/
+double raizCuadrada(double a);
+/*---------------      28. Salir ()      ------------------*/
 
